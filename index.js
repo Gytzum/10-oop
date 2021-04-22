bdf09af63dc0705856b26bcbc9bbbf1f9c93baee
Gytzum <gytzum@gmail.com>
2021-04-22 16:09:10 +0300

    Car

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('dasdddds');
+const Car = require('./js/Car.js');
+
+
+
+const volvo =new Car('Volvo', 'black');
+// const zaporazietis =new Car('Zapas', 'silver');
+// const tesla =new Car('Tesla', 'red');
+volvo.turnOn()
+volvo.drive(50)
+volvo.drive(15)
+volvo.turnOff()
+volvo.drive(20)
+
+
+// zaporazietis.changeColor('orange')
+// volvo.statusReport();
+// zaporazietis.statusReport();
+// tesla.statusReport();
